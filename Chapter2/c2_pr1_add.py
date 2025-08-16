e4b1e25d8d70dca38a6f64606175796ad4b3ab41
Pavan Vikhyath Kolluru <pavanvikhyathkolluru@gmail.com>
2025-08-16 16:10:11 +0530

    Chapter-2

---
+++ Chapter2/c2_pr1_add.py
@@ -0,0 +1,4 @@
+a = int(input("Enter number1: "))
+b = int(input("Enter number2: "))
+c = a + b
+print("The sum of", a, "and", b, "is", c)
---
+++ Chapter2/c2_pr2_rem.py
@@ -0,0 +1,4 @@
+z=int(input("enter a number to be stored in z:"))
+num=int(input("enter number that has to be divided by z:"))
+rem = float(num % z)
+print("The remainder of", num, "divided by", z, "is", rem)
